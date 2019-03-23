e65ecb5b0b6255d87683f7d9bdfc3a04220f40be
kelly tobar <kellytobar7@gmail.com>
2019-03-23 10:21:19 -0500

    exercise 5 with javascript object notation

--- js/ejercicios-reto3/ejercicio5.js
+++ js/ejercicios-reto3/ejercicio5.js
@@ -1,16 +1,23 @@
-function calculeYear() {
-    var year=document.getElementById("year").value;
-    
-    var res;
-    if(year<0){
-		$('.toast').toast('show');
-	}
-	if(year%4==0 && ( (year%100 ==0) && (year%400==0) )){
-		res="es bisiesto"
-	}
-	else{
-		res="no es bisiesto"
+	const leap ={
+	title:"Leap year",
+	calculateLeap: function(){
+		var year=document.getElementById("year").value;
+		var res;
+		if(year<0){
+			$('.toast').toast('show');
+		}
+		if(year%4==0 && ( (year%100 ==0) && (year%400==0) )){
+			res="es bisiesto"
+		}
+		else{
+			res="no es bisiesto"
+		}
+	
+		document.getElementById("result").innerHTML=`El año ${res}`;
+		}
 	}
 
-    document.getElementById("result").innerHTML=`El año ${res}`;
-    }
+	var calculate=document.getElementById("calculate");
+	calculate.addEventListener("click", leap.calculateLeap);
+	
+    
